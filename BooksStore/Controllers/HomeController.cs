b636d642468996b27e3cea96e11ebebc5971a05c
Your Name <you@example.com>
2022-06-12 01:35:14 +0700

    Add project files.

---
+++ BooksStore/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BooksStore.Models;
+using System.Linq;
+using BooksStore.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BooksStoreDbContext dbContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly BooksStoreDbContext _context;
+        private IBooksStoreRepository repository;
+        public int PageSize = 3;
+        public HomeController(IBooksStoreRepository repo, BooksStoreDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            repository = repo;
+            dbContext = context;
+            webHostEnvironment = hostEnvironment;
+            _context = context;
+        }
+        public IActionResult Index(string genre, int bookPage = 1)
+            => View(new BooksListViewModel
+            {
+
+                Books = repository.Books
+                    .Where(p => genre == null || p.Genre == genre)
+                    .OrderBy(b => b.BookID)
+                    .Skip((bookPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = bookPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = genre == null ?
+                    repository.Books.Count() : repository.Books.Where(e => e.Genre == genre).Count()
+
+                },
+                CurrentGenre = genre
+            });
+        public IActionResult Created()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Created(BooksViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = UploadedFile(model);
+
+                Book book = new Book
+                {
+
+                    Title = model.Title,
+                    Description = model.Description,
+                    Genre = model.Genre,
+                    Price = model.Price,
+                    ProfilePicture = uniqueFileName,
+                };
+                dbContext.Add(book);
+                await dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        private string UploadedFile(BooksViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.ImageBook != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "Images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImageBook.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ImageBook.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+    }
+}
---
+++ BooksStore/Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStore.Models.ViewModels
+{
+    public class BooksViewModel
+    {
+        [Key]
+        public long BookID { get; set; }
+        [Required(ErrorMessage = "Please enter a title")]
+        [Display(Name = "Tên sách")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter a description")]
+        [Display(Name = "Mô tả")]
+        public string Description { get; set; }
+        [Display(Name = "Giá")]
+        [Required]
+        [Range(0.01, double.MaxValue,
+        ErrorMessage = "Please enter a positive price")]
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Please specify a genre")]
+        [Display(Name = "Thể loại")]
+        public string Genre { get; set; }
+        [Required(ErrorMessage = "hãy chọn một bức hình")]
+        [Display(Name = "Hình ảnh sách")]
+        public IFormFile ImageBook{ get; set; }
+    }
+}
